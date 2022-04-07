ecae91866b6beeddaee55f502d74605f508dd522
brit444488 <brit444488@gmail.com>
2022-04-07 00:23:09 -0400

    initial commit

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, session, redirect
+
+app = Flask(__name__)
+app.secret_key = 'banana'
+
+@app.route("/")
+def index():
+    if 'visit' in session:
+        session['visit'] += 1
+    else:
+        session['visit'] = 1
+    if 'count' in session:
+        session['count'] += 1
+    else:
+        session['count'] = 0
+
+    return render_template('index.html')
+
+@app.route("/clear")
+def clear():
+    session.clear()
+    return redirect("/")
+
+
+
+if __name__ == '__main__':
+    app.run()
